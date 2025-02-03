2655adc619d63eddc083c671c8031471d8a1ee7e
exxidaus <kpinthawornrak@gmail.com>
2025-02-03 15:28:42 +0700

    feat: exercise04

---
+++ cell00/ex04/argv.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ $# -eq 0 ]
+then
+    echo -n "No arguments supplied"
+else
+    count=1
+    for arg in "$@"
+    do
+        if [ $count -eq $# ]
+        then
+            echo -n "$arg"
+        else
+            echo -e "$arg"
+        fi
+        count=$((count + 1))
+    done
+fi
